8fc19a910ce72280c5cb29eaf3e541e8bc39f5d8
Pål Grønås Drange <pgdr@statoil.com>
2016-12-06 21:46:55 +0100

    jumpers logic

--- sprite.py
+++ sprite.py
@@ -15,18 +15,26 @@ class Sprite(object):
         upd = self._velocity + s * self._acc/2
         self._pos += s * upd;
         self._velocity += s * self._acc;
-        if self._pos.y < 50:
-            self._pos.y = 50
-            self._velocity = self._velocity.flip_vertical(bp)
+        vx,vy = self._velocity.x, self._velocity.y
+        if self._pos.y < 70:
+            self._pos.y = 70
+            vy = 0
         if self._pos.x < 5:
             self._pos.x = 5.0
-            self._velocity = self._velocity.flip_horizontal(bp)
+            vx = 0
         if self._pos.x > 700:
             self._pos.x = 700.0
-            self._velocity = self._velocity.flip_horizontal(bp)
+            vx = 0
+        self._velocity = Coordinate(vx,vy)
 
     def jump(self):
-        self._velocity += Coordinate(0, 20)
+        if self._pos.x < 100:
+            self._velocity += Coordinate(500, 500)
+        elif self._pos.x > 600:
+            self._velocity += Coordinate(-500, 500)
+        elif self._pos.y < 100:
+            self._velocity += Coordinate(0, 250)
+        self._pos.y += 10
 
     def right(self):
         self._velocity += Coordinate(10,0)
